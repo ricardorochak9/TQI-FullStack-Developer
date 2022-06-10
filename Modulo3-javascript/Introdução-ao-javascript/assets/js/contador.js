c4a1595cfbd11026ab32af294241bef5f47dd495
mastersk <ricardorochak9@gmail.com>
2022-06-09 23:40:03 -0400

    att hoje

--- Modulo3-javascript/Introdução-ao-javascript/assets/js/contador.js
+++ Modulo3-javascript/Introdução-ao-javascript/assets/js/contador.js
@@ -1,16 +1,19 @@
 var currentNumberWrapper = document.getElementById('currentNumber');
 var currentNumber = 0;
+var cor = document.getElementById('currentNumber');
 
 function increment() {
     if (currentNumber < 10){ 
-        currentNumber = currentNumber + 1;
+        currentNumber++;
         currentNumberWrapper.innerHTML = currentNumber;
     }
 }
 
 function decrement() {
     if (currentNumber > -10){
-        currentNumber = currentNumber - 1;
+        currentNumber--;
         currentNumberWrapper.innerHTML = currentNumber;
     }
 }
+
+
